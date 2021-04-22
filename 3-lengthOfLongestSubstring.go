de7ecafa7748c54efe9074d593a5c8529613d4b2
honki <honkkki@yahoo.com>
2021-04-22 10:58:10 +0800

    3-无重复字符的最长子串

--- 3-lengthOfLongestSubstring.go
+++ 3-lengthOfLongestSubstring.go
@@ -2,15 +2,25 @@ package main
 
 import "fmt"
 
+// 滑动窗口
 func lengthOfLongestSubstring(s string) int {
 	if len(s) == 0 {
 		return 0
 	}
 
-	res, left, right := 0, 0, 0
-	for right < len(s) && left < len(s) {
-		right++
+	var freq [256]int
+	res, left, right := 0, 0, -1
 
+	for right < len(s) && left < len(s) {
+		if right+1 < len(s) && freq[s[right+1]-'a'] == 0 {
+			freq[s[right+1]-'a']++
+			right++
+		} else {
+			freq[s[left]-'a']--
+			left++
+		}
+
+		res = max(res, right-left+1)
 	}
 
 	return res
@@ -24,7 +34,7 @@ func max(a, b int) int {
 	return b
 }
 
-func main()  {
+func main() {
 	s := "abcdacd"
 	res := lengthOfLongestSubstring(s)
 	fmt.Println(res)
