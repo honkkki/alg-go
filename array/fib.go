9c8cc4e57a2fa50d5fbf5d023e6acf1fde7e98f2
honki <276086064@qq.com>
2021-07-30 17:57:37 +0800

    update

--- array/fib.go
+++ array/fib.go
@@ -4,8 +4,9 @@ import "fmt"
 
 var m = make(map[int]int)
 
+// 1 1 2 3 5 8
 func fib(n int) int {
-	if n < 2 {
+	if n <= 2 {
 		return 1
 	}
 
@@ -18,7 +19,24 @@ func fib(n int) int {
 	}
 }
 
+func fib2(n int) int {
+	if n <= 2 {
+		return 1
+	}
+
+	f1 := 1
+	f2 := 1
+	for i := 3; i <= n; i++ {
+		f3 := f1+f2
+		f1 = f2
+		f2 = f3
+	}
+
+	return f2
+}
+
 func main() {
-	res := fib(100)
+	res := fib(6)
 	fmt.Println(res)
+	fmt.Println(fib2(6))
 }
---
+++ linkedlist/206.go
@@ -0,0 +1,48 @@
+package main
+
+func reverseList(head *ListNode) *ListNode {
+	var arr = make([]int, 0, 10)
+	if head != nil {
+		for {
+			val := head.Val
+			arr = append(arr, val)
+			head = head.Next
+			if head == nil {
+				break
+			}
+		}
+	} else {
+		return nil
+	}
+
+	p := new(ListNode)
+	h := p
+	for i := len(arr) - 1; i >= 0; i-- {
+		p.Val = arr[i]
+		if i == 0 {
+			break
+		}
+		p.Next = new(ListNode)
+		p = p.Next
+	}
+
+	return h
+}
+
+func reverseList2(head *ListNode) *ListNode {
+	cur := head
+	pre := new(ListNode)
+	pre = nil
+	for cur != nil {
+		tmp := cur.Next
+		cur.Next = pre
+		pre = cur
+		cur = tmp
+	}
+
+	return pre
+}
+
+func main() {
+
+}
